cd4dc69ac5812edbcda8550d787f3719499a658f
Jacob Kahn <jgkahn33@gmail.com>
2020-08-13 19:39:09 -0400

    center up and down buttons on screen and add counter

--- clicker-react/src/components/admin/ClickerButtons.jsx
+++ clicker-react/src/components/admin/ClickerButtons.jsx
@@ -1,32 +1,62 @@
-import React from "react";
-import { Container, Header, Content, Button, Icon, View } from "native-base";
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Header,
+  Content,
+  Button,
+  Icon,
+  Card,
+  CardItem,
+  Body,
+  Text,
+  Grid,
+  Col,
+} from "native-base";
 
 export default function ClickerButtons() {
+  const [currentCount, setCurrentCount] = useState(0);
+
+  function decreaseCount() {
+    if (currentCount > 0) {
+      setCurrentCount(currentCount - 1);
+    }
+  }
+
   return (
     <Container>
       <Header />
-      <Content
-        contentContainerStyle={{
-          flexGrow: 0.25,
-          flexDirection: "column",
-          alignItems: "stretch",
-          justifyContent: "space-around",
-        }}
-      >
-        <Button
-          transparent
-          light
-          style={{ width: 50, height: 50, backgroundColor: "powderblue" }}
-        >
-          <Icon name='arrow-up' />
-        </Button>
-        <Button
-          transparent
-          light
-          style={{ width: 50, height: 50, backgroundColor: "skyblue" }}
-        >
-          <Icon name='arrow-down' />
-        </Button>
+      <Content contentContainerStyle={{ flex: 1 }} style={{ padding: 10 }}>
+        <Grid style={{ alignItems: "center" }}>
+          <Col>
+            <Button
+              onPress={() => setCurrentCount(currentCount + 1)}
+              transparent
+              light
+              style={{
+                alignSelf: "center",
+              }}
+            >
+              <Icon name='arrow-up' />
+            </Button>
+            <Text
+              style={{
+                alignSelf: "center",
+              }}
+            >
+              {currentCount}
+            </Text>
+            <Button
+              onPress={decreaseCount}
+              transparent
+              light
+              style={{
+                alignSelf: "center",
+              }}
+            >
+              <Icon name='arrow-down' />
+            </Button>
+          </Col>
+        </Grid>
       </Content>
     </Container>
   );
