845754038b7a650552296ad911939b565e0ed929
Jacob Kahn <jgkahn33@gmail.com>
2020-09-20 20:26:22 -0400

    add basic form into Profile screen

--- clicker-react/src/components/admin/Profile.jsx
+++ clicker-react/src/components/admin/Profile.jsx
@@ -1,6 +1,60 @@
-import React from 'react';
-import { View, Container, Text } from 'native-base';
+import React, { useState } from 'react';
+import { Label, Container, Item, Form, Input } from 'native-base';
+import { company } from '../../../mock-objects/company';
 
 export default function Profile() {
-  return <View></View>;
+  const [companyObj, setCompanyObj] = useState(company);
+
+  const updateCompany = (name, value) => {
+    setCompanyObj((companyObj) => ({ ...companyObj, [name]: value }));
+  };
+
+  return (
+    <Container>
+      <Form>
+        <Item stackedLabel>
+          <Label>Company Name</Label>
+          <Input
+            value={companyObj.name}
+            onChangeText={(text) => updateCompany('name', text)}
+          />
+        </Item>
+        <Item stackedLabel>
+          <Label>Address Line 1</Label>
+          <Input
+            value={companyObj.addressLine1}
+            onChangeText={(text) => updateCompany('addressLine1', text)}
+          />
+        </Item>
+        <Item stackedLabel>
+          <Label>Address Line 2</Label>
+          <Input
+            value={companyObj.addressLine2}
+            onChangeText={(text) => updateCompany('addressLine2', text)}
+          />
+        </Item>
+        <Item stackedLabel>
+          <Label>City</Label>
+          <Input
+            value={companyObj.city}
+            onChangeText={(text) => updateCompany('city', text)}
+          />
+        </Item>
+        <Item stackedLabel>
+          <Label>State</Label>
+          <Input
+            value={companyObj.stateOrProvince}
+            onChangeText={(text) => updateCompany('stateOrProvince', text)}
+          />
+        </Item>
+        <Item stackedLabel>
+          <Label>Zip</Label>
+          <Input
+            value={companyObj.postalCode}
+            onChangeText={(text) => updateCompany('postalCode', text)}
+          />
+        </Item>
+      </Form>
+    </Container>
+  );
 }
